ab2749eff7f8c86b070e6d5cea1c950219a7ce93
Sean Callan <seancallan@gmail.com>
2016-09-08 11:11:15 -0600

    Handle setup and installation on run

--- lib/nag.ex
+++ lib/nag.ex
@@ -2,6 +2,9 @@ defmodule Nag do
   use Application
 
   @cowboy_opts [ip: {0, 0, 0, 0}, port: 4000]
+  @file_permission 111
+  @repo_storage "./repos"
+  @script "./scripts/pronto.sh"
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
@@ -16,6 +19,7 @@ defmodule Nag do
   end
 
   def run do
+    setup
     print_server_location
     {:ok, _pid} = Plug.Adapters.Cowboy.http(Nag.Router, @cowboy_opts)
   end
@@ -25,4 +29,14 @@ defmodule Nag do
   defp print_server_location do
     IO.puts "Nag online at #{pretty_ip}:#{@cowboy_opts[:port]}"
   end
+
+  defp setup do
+    chmod_script
+    create_dir
+    install_gems
+  end
+
+  defp chmod_script, do: File.chmod(@script, @file_permission)
+  defp create_dir, do: File.mkdir(@repo_storage)
+  defp install_gems, do: Porcelain.shell("bundle install")
 end
